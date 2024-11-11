3ae75fee6794f4028562f2c2c69804a038afa08b
congc100 <2743196260@qq.com>
2024-11-11 17:52:58 +0200

    5.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    if (user !== null) {
+      blogService.getAll().then(blogs => setBlogs(blogs))
+    }
+  }, [user])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('login OK')
+    } catch (exception) {
+      console.log('login failed')
+    }
+  }
+
+  return user === null ?
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type='text' value={username} name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type='password' value={password} name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+    :
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <p>{user.name} logged in</p>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
-  )
 }
 
 export default App
