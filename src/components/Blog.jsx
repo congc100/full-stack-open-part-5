1b37242ef02e97965a09b91711df6025b1c70b7e
congc100 <2743196260@qq.com>
2024-11-15 12:52:02 +0200

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailVisible, setDetailVisible] = useState(false)
+
+  return <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author} &nbsp;
+      <button onClick={() => setDetailVisible(!detailVisible)}>
+        {detailVisible ? 'hide' : 'view'}
+      </button>
+    </div>
+    {detailVisible && <div>
+      <div>{blog.url}</div>
+      <div>
+        {blog.likes}
+        <button>like</button>
+      </div>
+      <div>{blog.user.name}</div>
+    </div>}
   </div>  
-)
+}
 
 export default Blog
